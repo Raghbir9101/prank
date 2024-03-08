0c2560f8dff0bc757d18c8334973af68cc998375
Raghbir Singh <rkgaming0203@gmail.com>
2024-03-08 20:41:39 +0530

    add

--- server.js
+++ server.js
@@ -2,12 +2,13 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const connection = mongoose.connect("mongodb+srv://admin:admin@cluster0.5zpelbd.mongodb.net/prank");
+const cors = require("cors");
 
 const schema = mongoose.Schema({
 
 }, { strict: false })
 const model = mongoose.model("prank", schema)
-
+app.use(cors())
 app.post("/postData", () => {
     const data = req.body;
     const newData = new model(data);
